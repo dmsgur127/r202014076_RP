bf6d5b86d0c7b98bf2b83c0b2acd1784edb62e43
Son, Eunhyeok <dmsgur12772@gmail.com>
2026-05-16 00:35:26 +0900

    State를 사용한 사용자 입력 관리(양방향 바인딩)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,23 @@
+import {useState} from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
+        <input type="text" onChange={handleChange} value={enteredPlayerName}/>
         <button>Set Name</button>
       </p>
     </section>
